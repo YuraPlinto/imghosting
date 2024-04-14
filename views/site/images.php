73543478ce0bbec245809f16b52510f1962c4a61
Plinto Yura <plinto.yura@yandex.ru>
2024-04-14 22:24:38 +0600

    Сделал просмотр оригинального изображения по ссылке со страницы со списком изображений

--- views/site/images.php
+++ views/site/images.php
@@ -9,17 +9,20 @@ $this->title = 'Images';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-images">
+    <p>Чтобы открыть картинку, кликните по превью.</p>
     <?php
     echo GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
             [
-                'label' => 'Превью',
+                'label' => 'Изображение',
                 'format' => 'html',
                 'contentOptions' => ['style' => 'width: 200px;'],
                 'value' => function ($data) {
                     $fileBaseName = mb_strstr($data->name, '.', true);
-                    return Html::img('@web/uploads/thumbnails/' . $fileBaseName . '.png', ['alt' => 'превью']);
+                    $href = '/uploads/' . $data->name;
+                    $previewImgTag = Html::img('@web/uploads/thumbnails/' . $fileBaseName . '.png', ['alt' => 'превью']);
+                    return \sprintf("<a href='%s'>%s</a>", $href, $previewImgTag);
                 }
             ],
             [
