31e84932d10c925e4087b9a39d56b751ae20e1d1
Plinto Yura <plinto.yura@yandex.ru>
2024-04-15 21:03:23 +0600

    Сделал автоматическую очистку файлов, связанных с удаляемой записью в БД

--- models/Image.php
+++ models/Image.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use Yii;
 use yii\db\ActiveRecord;
 
 class Image extends ActiveRecord
@@ -11,5 +12,22 @@ class Image extends ActiveRecord
         return [
             ['name', 'unique'],
         ];
+    }
+
+    public function afterDelete () {
+        parent::afterDelete();
+
+        $originalFile = Yii::$app->basePath . '/web/uploads/' . $this->name;
+        if (file_exists($originalFile))
+            unlink($originalFile);
+
+        $fileBaseName = mb_strstr($this->name, '.', true);
+        $thumbnailFile = Yii::$app->basePath . '/web/uploads/thumbnails/' . $fileBaseName . '.png';
+        if (file_exists($thumbnailFile))
+            unlink($thumbnailFile);
+
+        $archiveFile = Yii::$app->basePath . '/web/uploads/archive/' . $fileBaseName . '.zip';
+        if (file_exists($archiveFile))
+            unlink($archiveFile);
     }
 }
